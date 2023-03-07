30328135c780a2bd3a01166f8700216b88b8d3d8
JoshuaAbulu <abulujoshua99@gmail.com>
2023-03-07 16:45:32 +0100

    added components and meta tags in index.html

---
+++ little_lemon/src/Components/Header.js
@@ -0,0 +1,10 @@
+import whale from '../img/whale.jpg'
+const Header = () => {
+    return ( 
+        <header >
+            <img src={whale} alt="Whale" width='100' height='100'/>
+        </header>
+     );
+}
+ 
+export default Header;
