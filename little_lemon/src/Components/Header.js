82425819c27b2cb39e0379a4984e337f08242661
JoshuaAbulu <abulujoshua99@gmail.com>
2023-03-10 22:41:07 +0100

    added few things

--- little_lemon/src/Components/Header.js
+++ little_lemon/src/Components/Header.js
@@ -2,7 +2,7 @@ import logo from '../img/Logo.jpg'
 import Nav from './Nav';
 const Header = () => {
     return ( 
-        <header >
+        <header>
             <div className="container">
             <img src={logo} alt="Whale" width='150' height='30'/>
             <Nav />
--- little_lemon/src/Components/Main.js
+++ little_lemon/src/Components/Main.js
@@ -70,7 +70,7 @@ const Main = () => {
             <section className="title-page">
                 <div className="article1">
             <article className='art'>
-                <h1 className='tile1'>LITTLE LEMON</h1>
+                <h1 className='tile1' id='tile1'>LITTLE LEMON</h1>
                 <h3 className='tile2'>Chicago</h3>
                 <p className='tile3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid mollitia maxime quod aut inventore? Ipsam dolorum minima beatae. Est laboriosam cumque deserunt officia, possimus beatae sint neque sed non maiores assumenda ducimus perspiciatis cupiditate, eaque iste optio nisi aliquid libero enim id. Debitis beatae fugiat labore quod! Omnis, quo illum.</p>
                 <button className='gen-but'> <Link to='/table-reserve' className='navi2'> Reserve a Table </Link></button>
